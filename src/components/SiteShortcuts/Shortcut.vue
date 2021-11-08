5fd48b661ef8e7355d7be70af8d680645628be11
Sajad Hashemian <sajadhsm@gmail.com>
2021-11-08 13:02:05 +0330

    feat: ability to reorder shortcuts

---
+++ src/components/SiteShortcuts/Shortcut.vue
@@ -0,0 +1,54 @@
+<template>
+  <a :href="shortcut.url" :title="shortcut.name" class="shortcut">
+    <img
+      v-if="shortcut.hostname"
+      :src="`https://icons.duckduckgo.com/ip2/${shortcut.hostname}.ico`"
+      :alt="shortcut.name"
+      width="16"
+      height="16"
+      class="shortcut__favicon"
+    />
+    <i-ph-circle-dashed-bold v-else class="shortcut__no-favicon" />
+    <small class="shortcut__text">{{ shortcut.name }}</small>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'Shortcut',
+
+  props: {
+    shortcut: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.shortcut {
+  flex-direction: column;
+  margin-bottom: 10px;
+  padding: 5px;
+  text-decoration: none;
+}
+
+.shortcut__favicon {
+  margin-bottom: 4px;
+}
+.shortcut__no-favicon {
+  margin-bottom: 4px;
+  font-size: 16px;
+  color: hsla(var(--color-raw), 0.35);
+}
+
+.shortcut__text {
+  max-width: 40px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-size: 0.6rem;
+  color: hsla(var(--color-raw), 0.75);
+}
+</style>
---
+++ src/components/SiteShortcuts/Shortcuts.vue
@@ -0,0 +1,45 @@
+<template>
+  <Draggable
+    v-model="shortcuts"
+    :animation="200"
+    item-key="url"
+    ghost-class="shortcut--ghost"
+    @end="saveShortcutsToStorage"
+  >
+    <template #item="{ element }">
+      <Shortcut :shortcut="element" />
+    </template>
+  </Draggable>
+</template>
+
+<script>
+import Draggable from 'vuedraggable';
+
+import Shortcut from './Shortcut.vue';
+
+import useShortcuts from '@/composables/useShortcuts';
+
+export default {
+  name: 'Shortcuts',
+
+  components: {
+    Draggable,
+    Shortcut,
+  },
+
+  setup() {
+    const { shortcuts, saveShortcutsToStorage } = useShortcuts();
+
+    return {
+      shortcuts,
+      saveShortcutsToStorage,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.shortcut--ghost {
+  opacity: 0.5;
+}
+</style>
