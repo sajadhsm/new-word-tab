66c3fe3f8676dfdd5f5b78cf19e5a82c23e2f56a
Sajad Hashemian <sajadhsm@gmail.com>
2022-01-10 13:44:41 +0330

    feat: sort marked words by time

---
+++ src/components/MarkedWords/useSort.js
@@ -0,0 +1,44 @@
+import { ref, computed } from 'vue';
+
+import MdiSortClockAscending from '~icons/mdi/sort-clock-ascending';
+import MdiSortClockDescending from '~icons/mdi/sort-clock-descending';
+import MdiSortAlphabeticalAscending from '~icons/mdi/sort-alphabetical-ascending';
+import MdiSortAlphabeticalDescending from '~icons/mdi/sort-alphabetical-descending';
+
+const SORT_STATES = [
+  {
+    text: 'Time - Ascending',
+    icon: MdiSortClockAscending,
+    sortMethod: (list) => list,
+  },
+  {
+    text: 'Time - Descending',
+    icon: MdiSortClockDescending,
+    sortMethod: (list) => list.reverse(),
+  },
+  {
+    text: 'Alphabet - Ascending',
+    icon: MdiSortAlphabeticalAscending,
+    sortMethod: (list) => list.sort((a, b) => a.localeCompare(b)),
+  },
+  {
+    text: 'Alphabet - Descending',
+    icon: MdiSortAlphabeticalDescending,
+    sortMethod: (list) => list.sort((a, b) => b.localeCompare(a)),
+  },
+];
+
+export default function useSort() {
+  const sortState = ref(0);
+
+  const sortStateMeta = computed(() => SORT_STATES[sortState.value]);
+
+  function setNextSortState() {
+    sortState.value = (sortState.value + 1) % SORT_STATES.length;
+  }
+
+  return {
+    sortStateMeta,
+    setNextSortState,
+  };
+}
