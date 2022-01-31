e0d546b9bc44322b518ddcadb434a04f2e69d46b
Sajad Hashemian <sajadhsm@gmail.com>
2022-02-01 00:28:48 +0330

    chore: typescript-eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,20 @@
 module.exports = {
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint'],
   env: {
     node: true,
     webextensions: true,
+    'vue/setup-compiler-macros': true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
   rules: {
     'vue/multi-word-component-names': 'off',
   },
