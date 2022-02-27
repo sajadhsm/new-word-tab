ad03f665553bb1f2aa4b5a6bf06193b197dc33ba
Sajad Hashemian <sajadhsm@gmail.com>
2022-02-27 14:40:31 +0330

    chore: remove word actions positioning option

--- src/newTab/main.ts
+++ src/newTab/main.ts
@@ -3,5 +3,11 @@ import App from './App.vue';
 import '@/styles/normalize.css';
 import '@/styles/index.css';
 
+import storage from '@/modules/localStorage';
+
 const app = createApp(App);
 app.mount('#app');
+
+// Remove actions position from storage
+// It's temporarily and will be removed once all users upgraded to the next version
+storage.remove('ap')
